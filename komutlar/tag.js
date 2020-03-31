618188a6b1cc0b2c6ba4f96fc2f59eec1364d602
Glitch (hello-express) <none>
2020-03-31 20:22:12 +0000

    🔦🚖 Checkpoint ./komutlar/tag.js:3756271/215

--- komutlar/tag.js
+++ komutlar/tag.js
@@ -2,15 +2,15 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message) => {
   const yasak = client.emojis.get('693976996321165385');
-  if (message.channel.id !== '693286888341110885') return message.channel.send(`${yasak} **Bu Komutu <#> Gidip Yazmalısınız.**`).then(m => m.delete(5000));  
+  if (message.channel.id !== '693286888341110885') return message.channel.send(`${yasak} **Bu Komutu <#693286888341110885> Gidip Yazmalısınız.**`).then(m => m.delete(5000));  
   const elmas = client.emojis.get('694626540977127424');
-  const emoji = client.emojis.get('694626540977127424');
+  const emoji = client.emojis.get('694639615809814699');
       message.delete()
   message.channel.send('``∼``')
-  message.channel.send(`**Üstte Ve Altta Gördüğünüz Bize Ait Olan Tagımızdır..**`)
-  message.channel.send(`**Bu Tagı Kullanıcı Adınıza Koyarsanız RENK Değiştirme Özelliğini Kazanıyorsunuz.**`)
-  message.channel.send(`**Renk Değiştirmek İçin *<#693292261689131008>*.**`)
-  message.channel.send(`**Peki iyide Nasıl Kullanıcı Adıma Bunu Eklicem? Bunumu Soruyorsunuz.**`)
+  message.channel.send(`${emoji} **Üstte Ve Altta Gördüğünüz Bize Ait Olan Tagımızdır..**`)
+  message.channel.send(`${emoji} **Bu Tagı Kullanıcı Adınıza Koyarsanız RENK Değiştirme Özelliğini Kazanıyorsunuz.**`)
+  message.channel.send(`${emoji} **Renk Değiştirmek İçin *<#693292261689131008>*.**`)
+  message.channel.send(`${emoji} **Peki iyide Nasıl Kullanıcı Adıma Bunu Eklicem? Bunumu Soruyorsunuz.**`)
   message.channel.send('``∼``')
   const embed = new Discord.RichEmbed()
      .setColor('RANDOM')
@@ -23,7 +23,7 @@ ${elmas} **"HESABIM" Bölümünde Düzenleye Tıklayın**
 ${elmas} **Kullanıcı Adınızın Başına "∼" Tagını Ekleyin.**
 ${elmas} **Şifrenizi Yazıp Kaydete Tıklayın.**       
 `,true)
-        .setFooter(`Ailemize Katılmak Mı İstiyorsun? Devam Etmelisin...`)
+        .setFooter(`Ailemize Katılmak Mı İstiyorsun? Devam Etmelisin... Yardımlar Yolda Geliyor...`)
 message.channel.send(embed);
 };
 
