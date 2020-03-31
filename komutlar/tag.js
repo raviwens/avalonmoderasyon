07f04629cc2d50660674cb9b03fa7296ecde793a
Glitch (hello-express) <none>
2020-03-31 19:58:51 +0000

    🕴👜 Checkpoint ./komutlar/tag.js:3756271/276

--- komutlar/tag.js
+++ komutlar/tag.js
@@ -3,26 +3,25 @@ const Discord = require('discord.js');
 exports.run = async (client, message) => {
   const elmas = client.emojis.get('694626540977127424');
   const emoji = client.emojis.get('694626540977127424');
-  const emoji = client.emojis.get('694626540977127424');
+  const emoji2 = client.emojis.get('694626540977127424');
       message.delete()
-  message.channel.send(`∼`)
+  message.channel.send('``∼``')
   message.channel.send(`Bu Tagı Kullanıcı Adınıza Koyarsanız RENK Değiştirme Özelliğini Kazanıyorsunuz.`)
   message.channel.send(`Renk Değiştirmek İçin <#693292261689131008>.`)
   message.channel.send(`Peki iyide Nasıl Kullanıcı Adıma Bunu Eklicem? Bunumu Soruyorsunuz.`)
 const embed = new Discord.RichEmbed()
      .setColor('RANDOM')
     .setDescription(`
- ${}<@${message.author.id}> Yardımcı Oluyoruz Size :)
-${elmas} Sol Altta İsim ve Discord Numaranızın yanında Mikrofon Ses ve Ayarlar Menüsü var.
+ ${elmas}<@${message.author.id}> Yardımcı Oluyoruz Size :)
+
+${elmas} Sol Altta İsiminizin Yanında Mikrofon Ses ve Ayarlar Menüsü var.
 ${elmas} Ayarlar Menüsüne Tıklıyorsunuz.
 ${elmas} "HESABIM" Bölümünde Düzenleye Tıklayın
 ${elmas} Kullanıcı Adınızın Başına "∼" Tagını Ekleyin.
-${elmas} Şifrenizi Yazıp Kaydete Tıklayın
-                      
- 
+${elmas} Şifrenizi Yazıp Kaydete Tıklayın.       
 `,true)
-        .setFooter(`Vampir Köylü Oyunu..`)
-
+        .setFooter(`Ailemize Katılmak Mı İstiyorsun? Devam Etmelisin...`)
+message.channel.send(embed);
 };
 
 exports.conf = {
