93d01510eee4a3fa9079630105f950215ba45576
Glitch (hello-express) <none>
2020-04-30 13:40:04 +0000

    🗽🐴 Checkpoint ./komutlar/jail.js:3756271/2594

--- komutlar/jail.js
+++ komutlar/jail.js
@@ -1,87 +1,70 @@
 const Discord = require("discord.js");
 const ms = require("ms");
-const ayarlar = require('../ayarlar.json');
-const prefix = ayarlar.prefix;
 
 module.exports.run = async (client, message, args) => {
-let db = require('quick.db')
-let botisim = message.guild.members.get(client.user.id).displayName
-let data = await db.fetch(`jailrol_${message.guild.id}`)
-if(!data)  return message.channel.send(`Jail rolünü bulamadım.`)
-let data2 = await db.fetch(`jailyetkilisi_${message.guild.id}`)
-if(!data2)  return message.channel.send(`Jail yetkilisi rolünü bulamadım.`)
-let data3 = await db.fetch(`jailkanal_${message.guild.id}`)
-if(!data3)  return message.channel.send(`Jail kanalını bulamadım.`)
-let rol = message.guild.roles.get(data)
-if(!rol) return message.channel.send(`Jail rolü ayarlı değil.`)
-let yetkili = message.guild.roles.get(data2)
-if(!yetkili) return message.channel.send(`Jail yetkilisi ayarlı değil.`)
-let kanal = message.guild.channels.get(data3)
-if(!kanal) return message.channel.send(`Jail log kanalı ayarlı değil.`)
 
-  if (!message.member.roles.has(`${yetkili.id}`)) return message.channel.send(`**${ayarlar.prefix}jail** isimli komutu kullanabilmek için ${yetkili} rolüne sahip olman gerekiyor.`)
-  let kişi = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!kişi) return message.channel.send(`Kimi susturacaksın? Etiketlemeyi unutma.`)
-  if(kişi.hasPermission("MANAGE_GUILD")) return message.channel.send(`Olmaz. Bu kişiyi susturamam.`)
-  
-  let zaman = args[1]
-  if(!args[1]) return message.channel.send(`Ne kadar süre jailde duracağını belirtmelisin.\nÖrnek: +jail kişi süre sebep`)
+    if (!message.member.roles.find("name", "Yetkili")) {
+        return message.channel.send(' **Bu Komutu Kullanmak için** \*`Yetkili*\` **Rolüne Sahip Olman Lazım** ')
+            .then(m => m.delete(5000));
+    } 
+ let lozBey = message.mentions.members.first() || message.guild.members.get(args[0])
+  if(!lozBey) return message.channel.send("Lütfen susturulacak kişiyi etiketleyiniz.");
+  if(lozBey.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Benden yetkili birini susturamam.");
+  if (lozBey.id === message.author.id) return message.channel.send("Kendinizi susturamazsınız.");
+  let lozRol = message.guild.roles.find(`name`, "Susturuldu");
 
-let sebep = args.join(' ').slice(args[1].length+args[0].length + 1)
-if(!sebep) sebep = 'Sebep belirtilmemiş.'
-  
-  const wasted = new Discord.RichEmbed()
-  .setAuthor(message.author.tag, message.author.avatarURL)
-  .setColor(`#f3c7e1`)
-  .setDescription(`Al işte! Yine biri hapishaneye yollandı.`)
-  .addField(`**Hapishaneye yollanan kişi:**`, kişi, true)
-  .addField(`**Hakim:**`, `<@${message.author.id}>`, true)
-  .addField(`**Sebep:**`, sebep, true)
-  .addField(`**Süre:**`, zaman.replace(/s/, ' __saniye__').replace(/m/, ' __dakika__').replace(/h/, ' __saat__').replace(/d/, ' __gün__'), true)
-  .setTimestamp()
-  .setFooter(`${message.channel.name} kanalında kullanıldı.`)
-  .setThumbnail(message.author.avatarURL)
-  
-  const bitti = new Discord.RichEmbed()
-  .setAuthor(message.author.tag, message.author.avatarURL)
-  .setDescription(`Birisi tahliye oldu!`)
-  .addField(`**Tahliye olan:**`, kişi, true)
-  .addField(`**Hakim:**`, `<@${message.author.id}>`, true)
-  .setTimestamp()
-  .setColor(`#f3c7e1`)
-  .setFooter(`Jail süresi bitti. | ${message.channel.name} kanalında kullanıldı.`)
-  .setThumbnail(message.author.avatarURL)
-  
-  kişi.addRole(rol.id);
-    kişi.roles.forEach(r => {
-kişi.removeRole(r.id)
-db.set(`${message.guild.id}.jail.${kişi.id}.roles.${r.id}`, r.id )})
-    kanal.send(wasted)
-    message.channel.send(`${kişi} isimli kişi başarıyla hapishaneye gönderildi.`)
-    setTimeout(async () =>{
-    kişi.removeRole(rol.id)
-    kanal.send(bitti)
-  }, ms(zaman));
-            setTimeout(async () =>{
-message.guild.roles.forEach(async r => {
-const i = await db.fetch(`${message.guild.id}.jail.${kişi.id}.roles.${r.id}` )
-if(i != r.id)  return ;
-if(i){kişi.addRole(i)}
-})
-              }, ms(zaman));
+  if(!lozRol){
+    try{
+      lozRol = await message.guild.createRole({
+        name: "Susturuldu",
+        color: "#666666",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(lozRol, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
+
+  let efeZaman = args[1];
+  if(!efeZaman) return message.channel.send("Lütfen doğru bir zaman dilimi giriniz. Örneğin: ***!jail @kişi 1s/m/h/d sebep**");
+  let sebep = args[2]
+  if(!sebep) return message.channel.send("Lütfen bir sebep giriniz. Örneğin: ***!chatmute @kişi 1s/m/h/d sebep**");
+
+  await(lozBey.addRole(lozRol.id));
+   let embed = new Discord.RichEmbed()
+              .setAuthor(message.author.tag, message.author.displayAvatarURL)
+                .setDescription(` ${efeZaman} süreliğine  tarafından ${sebep} sebebiyle susturuldu!`)
+                .setColor("RANDOM");
+  message.channel.send(embed);
+
+  setTimeout(function(){
+    lozBey.removeRole(lozRol.id);
+    let sembed =  new Discord.RichEmbed()
+              .setAuthor(message.author.tag, message.author.displayAvatarURL)
+                .setDescription(` üyesinin, ${efeZaman} sürelik susturulması, otomatik olarak kaldırıldı.`)
+                .setColor("RANDOM");
+    message.channel.send(sembed);
+  }, ms(efeZaman));
+
+  message.delete();
+
 }
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['uçur'],
+    aliases: ["chat-mute","süreli-sustur"],
     permLevel: 0
-  };
-  
+};
+
 exports.help = {
- name: 'jail',
- description: 'Bir kişiyi belirlediğiniz rol ile jaile yollarsınız.',
- usage: 'jail @üye <10s,10m,10h,10d> sebep',
- kategori: '**MODERASYON**',
- permLvl: '**Bulunmuyor.** (+sustur-yetkilisi ayarla)'
+    name: 'sustur',
+    description: 'sustur',
+    usage: 'sustur'
 };
